f6a9491c2e0fe20172584c22e74f2a6a1fef8b2c
Banuka <banukaknight@gmail.com>
2020-09-05 14:28:09 +0530

    thebkrox

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -8,13 +8,6 @@ import { Component, OnInit } from '@angular/core';
 
   <h1> Event Handling</h1>
 
-  <button (click)=btn1_f($event)>GREET 1</button><br>{{greeting1}}<br>
-<button on-click=btn2_f($event)>GREET 2</button><br>{{greeting2}}<br>
-
-<button (click)=onDisplay($event)>Display</button><br>
-<input #myloginput type="text"><br>
-<button (click)=onLog(myloginput.value)>Log</button><br>
-<input #email (keyup.enter)=onKeyUp(email.value) type="email"/><br>
 
 <hr><hr>
 <input #num1 type="text">num1<br>
@@ -38,29 +31,6 @@ Sum = {{sum}}
 })
 export class EventsComponent implements OnInit {
 
-greeting1="";greeting2="";
-onKeyUp(value)
-{
-console.log(value);
-alert("hello World: "+value);
-}
-btn1_f(event){
-console.log(event);
-this.greeting1=event;
-}
-btn2_f(event){
-console.log(event);
-this.greeting2=event;
-}
-
-onDisplay(event){
-console.log(event);
-alert("hello World");
-}
-onLog(value){
-console.log("hello world");
-alert("Hello " +value);
-}
 
 //--------- calc stuff
 
